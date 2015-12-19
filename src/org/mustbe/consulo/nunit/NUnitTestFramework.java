5e3a5bc4821398a6b00a029c179eda66439d94fc
VISTALL <vistall.valeriy@gmail.com>
2015-12-19 20:01:35 +0300

    compilation

--- src/org/mustbe/consulo/nunit/NUnitTestFramework.java
+++ src/org/mustbe/consulo/nunit/NUnitTestFramework.java
@@ -16,64 +16,23 @@
 
 package org.mustbe.consulo.nunit;
 
-import javax.swing.Icon;
-
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.mustbe.consulo.RequiredReadAction;
 import org.mustbe.consulo.dotnet.psi.DotNetAttributeUtil;
-import org.mustbe.consulo.dotnet.psi.DotNetMethodDeclaration;
 import org.mustbe.consulo.dotnet.psi.DotNetTypeDeclaration;
+import org.mustbe.consulo.dotnet.run.DotNetTestFramework;
 import org.mustbe.consulo.nunit.module.extension.NUnitModuleExtension;
-import com.intellij.ide.fileTemplates.FileTemplateDescriptor;
-import com.intellij.lang.Language;
-import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
-import com.intellij.psi.PsiElement;
-import com.intellij.testIntegration.TestFramework;
-import com.intellij.util.IncorrectOperationException;
 
 /**
  * @author VISTALL
  * @since 10.02.14
  */
-public class NUnitTestFramework implements TestFramework
+public class NUnitTestFramework implements DotNetTestFramework
 {
-	@NotNull
+	@RequiredReadAction
 	@Override
-	public String getName()
-	{
-		return "NUnit";
-	}
-
-	@NotNull
-	@Override
-	public Icon getIcon()
-	{
-		return NUnitIcons.NUnit;
-	}
-
-	@Override
-	public boolean isLibraryAttached(@NotNull Module module)
-	{
-		return false;
-	}
-
-	@NotNull
-	@Override
-	public String getLibraryPath()
-	{
-		return "";
-	}
-
-	@Nullable
-	@Override
-	public String getDefaultSuperClass()
-	{
-		return null;
-	}
-
-	@Override
-	public boolean isTestClass(@NotNull PsiElement element)
+	public boolean isTestType(@NotNull DotNetTypeDeclaration element)
 	{
 		NUnitModuleExtension extension = ModuleUtilCore.getExtension(element, NUnitModuleExtension.class);
 		if(extension == null)
@@ -81,84 +40,6 @@ public class NUnitTestFramework implements TestFramework
 			return false;
 		}
 
-		if(element instanceof DotNetTypeDeclaration)
-		{
-			return DotNetAttributeUtil.hasAttribute(element, NUnitTypes.TestFixtureAttribute);
-		}
-		return false;
-	}
-
-	@Override
-	public boolean isPotentialTestClass(@NotNull PsiElement element)
-	{
-		return false;
-	}
-
-	@Nullable
-	@Override
-	public PsiElement findSetUpMethod(@NotNull PsiElement element)
-	{
-		return null;
-	}
-
-	@Nullable
-	@Override
-	public PsiElement findTearDownMethod(@NotNull PsiElement element)
-	{
-		return null;
-	}
-
-	@Nullable
-	@Override
-	public PsiElement findOrCreateSetUpMethod(@NotNull PsiElement element) throws IncorrectOperationException
-	{
-		return null;
-	}
-
-	@Override
-	public FileTemplateDescriptor getSetUpMethodFileTemplateDescriptor()
-	{
-		return null;
-	}
-
-	@Override
-	public FileTemplateDescriptor getTearDownMethodFileTemplateDescriptor()
-	{
-		return null;
-	}
-
-	@Override
-	public FileTemplateDescriptor getTestMethodFileTemplateDescriptor()
-	{
-		return null;
-	}
-
-	@Override
-	public boolean isIgnoredMethod(PsiElement element)
-	{
-		return false;
-	}
-
-	@Override
-	public boolean isTestMethod(PsiElement element)
-	{
-		NUnitModuleExtension extension = ModuleUtilCore.getExtension(element, NUnitModuleExtension.class);
-		if(extension == null)
-		{
-			return false;
-		}
-
-		if(element instanceof DotNetMethodDeclaration)
-		{
-			return DotNetAttributeUtil.hasAttribute(element, NUnitTypes.TestAttribute);
-		}
-		return false;
-	}
-
-	@NotNull
-	@Override
-	public Language getLanguage()
-	{
-		return Language.ANY;
+		return DotNetAttributeUtil.hasAttribute(element, NUnitTypes.TestFixtureAttribute);
 	}
 }
