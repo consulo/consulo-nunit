65a3d9d5db7e1d5216fbf90ed36b196526f20169
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-11 14:59:11 +0400

    compile fix

--- plugin/src/main/java/consulo/nunit/impl/run/NUnitConfigurationProducer.java
+++ plugin/src/main/java/consulo/nunit/impl/run/NUnitConfigurationProducer.java
@@ -20,66 +20,55 @@ import consulo.annotation.component.ExtensionImpl;
 import consulo.execution.action.ConfigurationContext;
 import consulo.execution.action.RunConfigurationProducer;
 import consulo.language.psi.PsiElement;
-import consulo.language.util.ModuleUtilCore;
 import consulo.module.Module;
 import consulo.nunit.module.extension.NUnitModuleExtension;
-import consulo.util.lang.ref.Ref;
+import consulo.util.lang.ref.SimpleReference;
 
 /**
  * @author VISTALL
  * @since 26.07.14
  */
 @ExtensionImpl
-public class NUnitConfigurationProducer extends RunConfigurationProducer<NUnitConfiguration>
-{
-	public NUnitConfigurationProducer()
-	{
-		super(NUnitConfigurationType.getInstance().getConfigurationFactories()[0]);
-	}
+public class NUnitConfigurationProducer extends RunConfigurationProducer<NUnitConfiguration> {
+    public NUnitConfigurationProducer() {
+        super(NUnitConfigurationType.getInstance().getConfigurationFactories()[0]);
+    }
 
-	@Override
-	protected boolean setupConfigurationFromContext(NUnitConfiguration configuration, ConfigurationContext context, Ref<PsiElement> sourceElement)
-	{
-		PsiElement psiLocation = context.getPsiLocation();
-		if(psiLocation == null)
-		{
-			return false;
-		}
+    @Override
+    protected boolean setupConfigurationFromContext(NUnitConfiguration configuration, ConfigurationContext context, SimpleReference<PsiElement> sourceElement) {
+        PsiElement psiLocation = context.getPsiLocation();
+        if (psiLocation == null) {
+            return false;
+        }
 
-		Module moduleForRun = getModuleForRun(context);
-		if(moduleForRun != null)
-		{
-			configuration.setName(moduleForRun.getName());
-			configuration.setModule(moduleForRun);
-			return true;
-		}
-		return false;
-	}
+        Module moduleForRun = getModuleForRun(context);
+        if (moduleForRun != null) {
+            configuration.setName(moduleForRun.getName());
+            configuration.setModule(moduleForRun);
+            return true;
+        }
+        return false;
+    }
 
-	@Override
-	public boolean isConfigurationFromContext(NUnitConfiguration configuration, ConfigurationContext context)
-	{
-		Module moduleForRun = getModuleForRun(context);
-		if(moduleForRun == null)
-		{
-			return false;
-		}
+    @Override
+    public boolean isConfigurationFromContext(NUnitConfiguration configuration, ConfigurationContext context) {
+        Module moduleForRun = getModuleForRun(context);
+        if (moduleForRun == null) {
+            return false;
+        }
 
-		return configuration.getConfigurationModule().getModule() == moduleForRun;
-	}
+        return configuration.getConfigurationModule().getModule() == moduleForRun;
+    }
 
-	private static Module getModuleForRun(ConfigurationContext configurationContext)
-	{
-		Module module = configurationContext.getModule();
-		if(module == null)
-		{
-			return null;
-		}
-		NUnitModuleExtension extension = ModuleUtilCore.getExtension(module, NUnitModuleExtension.class);
-		if(extension != null)
-		{
-			return module;
-		}
-		return null;
-	}
+    private static Module getModuleForRun(ConfigurationContext configurationContext) {
+        Module module = configurationContext.getModule();
+        if (module == null) {
+            return null;
+        }
+        NUnitModuleExtension extension = module.getExtension(NUnitModuleExtension.class);
+        if (extension != null) {
+            return module;
+        }
+        return null;
+    }
 }
